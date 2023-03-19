d297c55209f78b99384ba1e391be6638b9d13c79
panji gntg <127183420+oktapanjiwinata@users.noreply.github.com>
2023-03-19 18:08:14 +0700

    Add files via upload

---
+++ employe_delete.php
@@ -0,0 +1,24 @@
+<?php 
+
+   require 'functionemploye.php';
+
+$id = $_GET["id"];
+
+if(hapus($id) >0 ){
+	echo "
+				<script>
+					alert('data berhasil dihapus!');
+					document.location.href = 'employe.php';
+				</script>
+		";
+	} else {
+		echo "
+		<script>
+					alert('data gagal dihapus!');
+					document.location.href = 'employe.php';
+				</script>
+		";
+	}
+
+
+ ?>
